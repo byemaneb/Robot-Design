4a2b7ed641c95019b338bc0fe457732287895f43
byemaneb <47338463+byemaneb@users.noreply.github.com>
2019-03-19 22:43:38 -0400

    Update SerialTest.ino
    make sure that arduino can parse integers regardless of sign

--- Arduino/SerialTest/SerialTest.ino
+++ Arduino/SerialTest/SerialTest.ino
@@ -6,20 +6,28 @@ void setup() {
 }
 
 void loop() { // run over and over
-
-  Convert();
-  delay(2000);
+  if (Serial1.available() > 0) {
+    Convert();
+  }
+  delay(1000);
 }
 
 void Convert(void) {
   int incomingByte = 0;
   int integerValue = 0;
+  int sign = 1;
   while (1) {                                                                   // force into a loop until 'n' is received
     incomingByte = Serial1.read();                                               // byte that was read
     if (incomingByte == '\n') break;                                            // exit the while(1), we're done receiving
-    integerValue *= 10;                                                         // shift left 1 decimal place
-    integerValue = ((incomingByte - 48) + integerValue);                        // convert ASCII to integer, add, and shift left 1 decimal place
-    
+    if (incomingByte == 45) {
+      sign= -1;
+    } else {
+      integerValue *= 10;
+      integerValue = ((incomingByte - 48) + integerValue);                        // convert ASCII to integer, add, and shift left 1 decimal place
+    }
+
   }
-  Serial.println(integerValue);
+  integerValue = integerValue * sign;
+    Serial.println(integerValue);
+
 }
