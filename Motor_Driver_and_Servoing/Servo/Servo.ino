5d344a4bda18ca5554be2ee040e07d7c8f925333
byemaneb <47338463+byemaneb@users.noreply.github.com>
2019-04-09 21:30:38 -0400

    Update Servo.ino
    created a funtioning state machine

--- Motor_Driver_and_Servoing/Servo/Servo.ino
+++ Motor_Driver_and_Servoing/Servo/Servo.ino
@@ -1,3 +1,13 @@
+/*
+
+
+*/
+/////////////////////////////////////////////////////////////////
+
+// Global variables
+
+#define MAX_ANGLE 720
+#define MIN_ANGLE -720
 
 #define FORWARD_MOTOR_PIN 9
 #define REVERSE_MOTOR_PIN 10
@@ -5,112 +15,97 @@
 #define BOTTOM_IR_SENSOR_INTERUPPT_PIN 16
 #define TOP_IR_SENSOR_INTERUPPT_PIN 15
 
+#define FORWARD 1
+#define REVERSE -1
 
-// variables
-int tick;
-int desiredPosition;
-int currentPosition;
-int   sensorA;
-int   sensorB;
-int state;
-int currentState;
-int previousState;
-int motorDirection;
+#define DEGREES_PER_TICK  2.4
 
+/////////////////////////////////////////////////////////////////
+
+//variables
+
+int desiredAngle = 0;
+int currentAngle = 0;
+
+int currentTick;
+int desiredTick = 0;
+int tick = 0;
+
+int error = 0;
+
+int BOTTOM_IR_SENSOR = 0;
+int TOP_IR_SENSOR = 0;
+
+int currentState = 0;
+int previousState = 0;
+
+int turnDirection = 0;
+
+/////////////////////////////////////////////////////////////////
+
+// methods used
+
+//motor direction
+void motorDirection(void) {
+  if ((currentState == 1) && (previousState == 3)) {
+    turnDirection = FORWARD;
+  }
+
+  if ((currentState == 2) && (previousState == 3)) {
+    turnDirection = REVERSE;
+  }
+}
+
+// state of the encoder wheel
+void encoderState(void) {
+  TOP_IR_SENSOR = digitalRead(TOP_IR_SENSOR_INTERUPPT_PIN);
+  BOTTOM_IR_SENSOR = digitalRead(BOTTOM_IR_SENSOR_INTERUPPT_PIN);
+
+  if ( (BOTTOM_IR_SENSOR == 0) && (TOP_IR_SENSOR == 0)) {
+    currentState = 0;
+  } else if ( (BOTTOM_IR_SENSOR == 1) && (TOP_IR_SENSOR == 0) ) {
+    currentState = 1;
+  } else if ( (BOTTOM_IR_SENSOR == 0) && (TOP_IR_SENSOR == 1) ) {
+    currentState = 2;
+  } else if ( (BOTTOM_IR_SENSOR == 1) && (TOP_IR_SENSOR == 1) ) {
+    currentState = 3;
+  }
+}
+
+void counter(void) {
+  tick++;
+}
+
+/////////////////////////////////////////////////////////////////
+
+//put your setup code
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  pinMode(FORWARD_MOTOR_PIN, OUTPUT);                                            // init the digital pin as output for motor control
-  pinMode(REVERSE_MOTOR_PIN, OUTPUT);                                            // init the digital pin as output for motor control
 
-  pinMode(BOTTOM_IR_SENSOR_INTERUPPT_PIN, INPUT_PULLUP);                        // init inturrupt pin
-  attachInterrupt(digitalPinToInterrupt(TOP_IR_SENSOR_INTERUPPT_PIN), topSensorOn, RISING); // attach interupt to a function
-  //attachInterrupt(digitalPinToInterrupt(BOTTOM_IR_SENSOR_INTERUPPT_PIN), bottomSensorOn, RISING); // attach interupt to a function
+  //initialize pins
+  pinMode(FORWARD_MOTOR_PIN, OUTPUT);
+  pinMode(REVERSE_MOTOR_PIN, OUTPUT);
 
-  //attachInterrupt(digitalPinToInterrupt(TOP_IR_SENSOR_INTERUPPT_PIN), topSensorOff, LOW); // attach interupt to a function
-  //attachInterrupt(digitalPinToInterrupt(TOP_IR_SENSOR_INTERUPPT_PIN), bottomSensorOff, FALLING); // attach interupt to a function
+  //initialize pins at input
+  pinMode(TOP_IR_SENSOR_INTERUPPT_PIN, INPUT);
+  pinMode(BOTTOM_IR_SENSOR_INTERUPPT_PIN, INPUT);
+  //pinMode(BOTTOM_IR_SENSOR_INTERUPPT_PIN, INPUT_PULLUP);
 
-  //desiredPosition = 0;                                                   // init desiredPosition as 0 ; until user defines another one
-  //tick = 0;                                                             // intit the tick counter for the IR sensor
-
-  sensorA = 0;
-  sensorB = 0;
-  motorDirection = FORWARD_MOTOR_PIN;
-  state = 0;
-}
-
-
-
-void loop() { // run over and over
-  // if there is a value in the
-  //Serial.println("desiredPosition");
-
-  if (Serial.available() > 0) {
-    desiredPosition = Convert();
-    Serial.println("desiredPosition");
-    Serial.println(desiredPosition);
-    tick = 0;
-
-  }
-  
-    while(tick < desiredPosition ){
-      turnMotor(100);
-      Serial.println(tick);
-    }
-  
-
-
-
-
-  turnMotor(0);
-  delay(500);
-}
-
-// convert inputed serial data as a integer value
-int Convert(void) {
-  int incomingByte = 0;
-  int integerValue = 0;
-  int sign = 1;
-  while (1) {                                                                   // force into a loop until 'n' is received
-    incomingByte = Serial.read();                                               // byte that was read
-    if (incomingByte == '\n') break;                                            // exit the while(1), we're done receiving
-    if (incomingByte == 45) {
-      sign = -1;                                                                // make value negative
-    } else {
-      integerValue *= 10;
-      integerValue = ((incomingByte - 48) + integerValue);                        // convert ASCII to integer, add, and shift left 1 decimal place
-    }
-
-  }
-  integerValue = integerValue * sign;                                           //
-  return integerValue;
-  //Serial.println(integerValue);
-}
-
-//
-void topSensorOn(void) {
-  tick++;
- // sensorA = !sensorA;
-  //Serial.println("top");
-  //Serial.println(sensorA);
+  // attach interupt to a function
+  attachInterrupt(digitalPinToInterrupt(TOP_IR_SENSOR_INTERUPPT_PIN), counter, RISING);
+  //attachInterrupt(digitalPinToInterrupt(TOP_IR_SENSOR_INTERUPPT_PIN), topSensorTrigger, RISING);
 
 }
-void topSensorOff(void) {
-  Serial.println("top off");
-}
-void bottomSensorOn(void) {
+/////////////////////////////////////////////////////////////////
 
-  sensorB = !sensorB;
-  //Serial.println("bottom");
-  //Serial.println(sensorB);
+// main code
+void loop() {
 
-}
-void bottomSensorOff(void) {
-  Serial.println("bottom off");
-}
+  // case where
+  encoderState();
+  motorDirection();
+  previousState  = currentState;
 
-//setMotor
-void  turnMotor(int angle) {
-  analogWrite(motorDirection, angle); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
 
 }
