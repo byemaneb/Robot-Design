cc7e2c20b6da5f67bd254b3737b0731b2f93e82a
byemaneb <47338463+byemaneb@users.noreply.github.com>
2019-04-26 07:23:04 -0400

    Update udp_receive.py

--- Robot_Code/UDPTestPi/udp_receive.py
+++ Robot_Code/UDPTestPi/udp_receive.py
@@ -6,14 +6,16 @@ import struct
 UDP_IP = "172.20.10.8"
 UDP_PORT = 5005
 
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+
 while True:
 	data,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-	#speed,angle = struct.unpack("!ff",data)
-	#print("speed",speed)
+
+	print("speed",struct.unpack("@fff",data))
+	#print("angle",turn)
 	#print("angle",angle)
-	print("recieved message is ",data)
 	
 	
