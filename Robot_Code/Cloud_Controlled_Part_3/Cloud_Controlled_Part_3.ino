46142d7366442af5503c6857a780b2f5e60964e0
byemaneb <47338463+byemaneb@users.noreply.github.com>
2019-05-04 18:38:12 -0400

    Create Cloud Controlled Part 3

---
+++ Robot_Code/Cloud_Controlled_Part_3/Cloud_Controlled_Part_3.ino
@@ -0,0 +1,29 @@
+#include  <Arduino.h>
+#include  "SourceCode.h"
+
+int counter = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  connectWifi();
+  initializePorts();
+
+}
+
+void loop() {
+  
+  orientation();  // read in heading data
+  eadUDP();      // open UDP port and read in UDP data
+
+  pickUp();       // pickUp = TRUE if thrshhold is reached
+  
+  // check UDP angle data
+  if(((dataIn->robotAngle) == 90)||((dataIn->robotAngle) == 180)||((dataIn->robotAngle) == 270)||((dataIn->robotAngle) == 360)){
+    rotate();     // rotate robot until wanted orientation is reached
+  }else{
+    driveRobot();     // drive robot to UDP provided speed
+  }
+
+
+}
